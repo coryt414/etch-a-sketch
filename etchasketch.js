29e5b9a91040eb8deb3d69fbb2a008274e8dcefa
Cory Thomas <coryt414@gmail.com>
2024-06-04 12:03:05 -0400

    Add opacity scaling on mouseover

--- etchasketch.js
+++ etchasketch.js
@@ -9,7 +9,6 @@ function createGrid(width, height) {
         rowDiv.style.display = 'flex';
         for (let j = 0; j < width; j++) {
             const individualBox = document.createElement("div");
-            individualBox.textContent = "test";
             individualBox.classList.add("individualBox");
             rowDiv.appendChild(individualBox);
         }
@@ -23,14 +22,36 @@ function createGrid(width, height) {
 
 let btn = document.querySelector('#resize');
 btn.addEventListener('click', function (e) {
-    width = prompt("Please enter a width", "16");
-    height = prompt ("Please enter a height", "16");
+    inputWidth = '';
+    inputWidth = prompt("Please enter a width between 1 - 100", "16");
+    while (isNaN(inputWidth) || inputWidth < 1 || inputWidth > 100) {
+        alert("Please enter a valid number")
+        inputWidth = prompt("Please enter a width between 1 - 100", "16");
+    }
+    width = inputWidth;
+    inputHeight = '';
+    inputHeight = prompt("Please enter a height between 1 - 100", "16");
+    while (isNaN(inputHeight) || inputHeight < 1 || inputHeight > 100) {
+        alert("Please enter a valid number")
+        inputHeight = prompt("Please enter a width between 1 - 100", "16");
+    }
+    height = inputHeight;
     container.innerHTML = "";
     createGrid(width,height);
 });
 
 function colorOnHover(e) {
-    e.target.style.background = "red";
+    currentOpacity = e.target.style.opacity;
+    console.log(e.target.style.opacity)
+    if (currentOpacity === '') {
+        e.target.style.opacity = 0.25;
+    }
+    else {
+        e.target.style.opacity = parseFloat(currentOpacity) + .25;
+    }
+    e.target.style.background = "teal";
+    console.log(currentOpacity)
+    console.log(e.target.style.opacity)
 };
 
 createGrid(16,16);
